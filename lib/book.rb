43c8979303ca6d62a86f4dfcc9ff6923a08592c5
Chris Wiker <cwiker7@gmail.com>
2022-01-13 10:15:36 -0500

    commit tree fe98d897374677230ecb2c64cf29d56ac14c241b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_reader :title, :author_name, :page_count, :genre 
+    
+    def initialize(title)
+        @title = title
+        @author_name = author_name
+        @page_count = page_count
+        @genre = genre
+    end
+    
+    def author_name
+    end
+    
+    # def author_name=(author_name)
+    #     @author_name = author_name
+    # end 
 
+    # def author_name
+    #     @author_name 
+    # end
+    
+    # def initialize(author)
+    #     @author_name = author_name
+    # end
+
+end  
 
